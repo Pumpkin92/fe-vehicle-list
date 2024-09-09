d93a5a6a09b0b5da66e8f8bc33d77b38c93192d6
Pumpkin92 <77683675+Pumpkin92@users.noreply.github.com>
2024-09-09 23:05:18 +0100

    completes mobile first styling

--- src/components/VehicleCard.tsx
+++ src/components/VehicleCard.tsx
@@ -1,23 +1,55 @@
+import { useState } from "react";
+
 export default function VehicleCard({ vehicle }: object) {
+  const [favourite, setFavourite] = useState(false);
   return (
     <>
       <div className="vehicle-container">
-        <img src={vehicle.original_media_urls[0]} />
-        <p>
-          {vehicle.plate} {vehicle.make} {vehicle.model}
-        </p>
-        <div className="ad-classification">{vehicle.advert_classification}</div>
-        <div>Star</div>
-        <p>{vehicle.derivative}</p>
-        <ul>
-          {vehicle.key_features.map((feature) => {
-            return <li>{feature}</li>;
+        <div className="img-container">
+          {vehicle.media_urls.map((img: any) => {
+            return <img className="car-img" src={img.thumb} />;
           })}
-        </ul>
-        <p>
-          {vehicle.monthly_payment} /mo {vehicle.monthly_finance_type}
-        </p>
-        <p>£{vehicle.price_when_new}</p>
+        </div>
+        <div className="car-main-info">
+          <p>
+            {vehicle.plate} {vehicle.make} {vehicle.model}
+          </p>
+          <div className="classification-container">
+            <div className="classification-box">
+              <p className="vehicle-classification">
+                {vehicle.advert_classification}
+              </p>
+            </div>
+            <div
+              className={`star ${favourite ? "active" : ""}`}
+              onClick={() => {
+                setFavourite(!favourite);
+              }}
+            >
+              ★
+            </div>
+          </div>
+        </div>
+        <p className="vehicle-derivative">{vehicle.derivative}</p>
+        <div className="vehicle-details">
+          <ul className="mapped-feature-elements">
+            {vehicle.key_features.map((feature, index) => {
+              return (
+                <>
+                  <li key={index}>{feature}</li>
+                  <span className="divider">|</span>
+                </>
+              );
+            })}
+          </ul>
+
+          <div className="vehicle-pricing">
+            <p className="price-monthly">
+              £{vehicle.monthly_payment} /mo {vehicle.monthly_finance_type}
+            </p>
+            <p className="price-total">£{vehicle.price_when_new}</p>
+          </div>
+        </div>
       </div>
     </>
   );
